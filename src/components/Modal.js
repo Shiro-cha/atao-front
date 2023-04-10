bf8bc86b446f79782bc4e1acb0fc584d068f6467
Shiro-cha <noum.rzdr@gmail.com>
2023-04-10 03:13:45 +0300

    Message by stp

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-
+import moment from 'moment';
 
 import IconButton  from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
@@ -58,6 +58,27 @@ export default function MyModal({open,handleClose}) {
           />
           
           <CardContent className="card-body">
+          
+          <form onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.elements.title.value;
+        const date = e.target.elements.date.value;
+        const time = e.target.elements.time.value;
+        const start = moment(`${date} ${time}`, 'YYYY-MM-DD HH:mm').toDate();
+        const end = moment(start).add(1, 'hour').toDate();
+        addEvent(title, start, end);
+      }}>
+        <label htmlFor="title">Titre :</label>
+        <input type="text" name="title" required />
+
+        <label htmlFor="date">Date :</label>
+        <input type="date" name="date" required />
+
+        <label htmlFor="time">Heure :</label>
+        <input type="time" name="time" required />
+
+        <button type="submit">Ajouter</button>
+      </form>
           
           </CardContent>
        </Card>
