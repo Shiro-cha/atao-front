b52dfa61fc0cf304c00b169383cc5a607d1639b2
Shiro-cha <noum.rzdr@gmail.com>
2023-04-10 18:24:54 +0300

    Message by stp

--- src/components/ColocComponent.js
+++ src/components/ColocComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import List from '@mui/material/List';
+import Card from '@mui/material/Card';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
@@ -26,7 +26,7 @@ export default function ColocComponent() {
 
   return (
   <main  id="main" className="main">
-    <List >
+    <Card >
       {roommateList.map((roommate, index) => (
         <React.Fragment key={index}>
           <ListItem alignItems="flex-start" button>
@@ -40,7 +40,7 @@ export default function ColocComponent() {
           {index !== roommateList.length - 1 && <Divider variant="inset" component="li" />}
         </React.Fragment>
       ))}
-    </List>
+    </Card>
     </main>
   );
 }
