4f4744f0bed823b7f14248a2ff1188bb9eaeabc5
Shiro-cha <noum.rzdr@gmail.com>
2023-04-10 20:54:23 +0300

    Message by stp

--- src/components/EmploisDuTempsTable.js
+++ src/components/EmploisDuTempsTable.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
  
 import  Table from '@mui/material/Table';
-import  TableBody from '@mui/material/TableCell';
-import  TableCell from '@mui/material';
+import  TableBody from '@mui/material/TableBody';
+import  TableCell from '@mui/material/TableCell';
 import  TableContainer from '@mui/material/TableContainer';
 import  TableHead from '@mui/material/TableHead';
 import  TableRow from '@mui/material/TableRow';
