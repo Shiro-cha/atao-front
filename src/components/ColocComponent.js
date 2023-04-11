0dc095937c2d73973dd6a5fa7ef578be4d9a199b
Shiro-cha <noum.rzdr@gmail.com>
2023-04-11 05:53:18 +0300

    Message by stp

--- src/components/ColocComponent.js
+++ src/components/ColocComponent.js
@@ -1,4 +1,4 @@
-import {useEffect,useState} from 'react';
+import React,{useEffect,useState} from 'react';
 
 import Paper from '@mui/material/Paper';
 import ListItem from '@mui/material/ListItem';
