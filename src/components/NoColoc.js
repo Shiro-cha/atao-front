0afa0893edfea4185796d7bd221fa5106fbd4d79
Shiro-cha <noum.rzdr@gmail.com>
2023-04-11 06:24:00 +0300

    Message by stp

--- src/components/NoColoc.js
+++ src/components/NoColoc.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { useCookies } from 'react-cookie';
 import Button from "@mui/material/Button";
+import axios from 'axios';
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
@@ -7,12 +9,25 @@ import DialogActions from "@mui/material/DialogActions";
 import Typography  from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 
-import SearchColoc from "./SeachColoc"
+import SearchColoc from "./SeachColoc";
+
+import Toast from "./Toast";
+
+import baseUrl from "../config/baseUrl";
+
+let api = axios.create(baseUrl)
 
 const NoRoommate = ({user_id}) => {
+    const [openToast, setOpenToast] = useState(false);
+    const [typeToast, setTypeToast] = useState("success");
 
-   
+    const handleCloseToast = (evt) => {
 
+        setOpenToast(false);
+
+    }
+
+   const [cookies] = useCookies(['users']);
   const [openJoinDialog, setOpenJoinDialog] = useState(false);
   const [openCreateDialog, setOpenCreateDialog] = useState(false);
   const [colocataire, setColocataire] = useState("");
@@ -45,10 +60,26 @@ const NoRoommate = ({user_id}) => {
   
   const createColoc =() =>{
   
+  api.post('/create-coloc', {
+      id_members:[cookies.users],
+      nom_coloc:colocataire
+    })
+    .then(response => {
+      setTypeToast("success")
+         setOpenToast(true)
+      
+    })
+    .catch(error => {
+      // handle error
+      setTypeToast("error")
+         setOpenToast(true)
+    });
+  
   }
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+       <Toast open={openToast} handleClose={handleCloseToast} type={typeToast} />
       <Typography variant="p" sx={{color:"#333333"}}>Vous n'avez pas encore de colocataire</Typography>
       <div className="mt-5 row">
       <div className="col-6">
